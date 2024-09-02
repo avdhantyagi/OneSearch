9dde97364310958f303ba13d6b645fdbf4526e38
avdhantyagi <avdhantyagi1011@gmail.com>
2024-09-03 02:35:02 +0530

    Elements showing up in phone

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -11,14 +11,14 @@ export default function Page() {
     const query = searchParams.get('query')
    
     return (
-    <div className='flex flex-col min-h-screen items-center max-w-[800px]'>
+    <div className='flex flex-col min-h-screen items-center max-w-[800px] min-w-[200px]'>
         <div className='text-white new-amsterdam-regular lg:text-8xl lg:mt-[15vh] lg:mb-8' >OneSearch</div>
         <div className='flex flex-row justify-center w-full'>
             <Category type='Search'/>
             <Category type='Gen AI'/>
             <Category type='Streaming'/>
         </div>
-        <div className='grid sm:grid-cols-1 sm:w-[400px] md:min-w-[600px] lg:w-[800px] lg:grid:cols-2 gap-4 md:grid-cols-2 p-4 min-h-[50vh]'>
+        <div className='grid w-[150px] sm:grid-cols-1 sm:min-w-[400px] md:min-w-[600px] lg:w-[800px] lg:grid:cols-2 gap-4 md:grid-cols-2 p-4 min-h-[50vh]'>
             <Card />
             <Card />
             <Card />
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Card() {
   return (
-    <div className='bg-gray-800 rounded-lg lg:h-48'>
+    <div className='bg-gray-800 rounded-lg sm:h-10 lg:h-48'>
     </div>
   )
 }
